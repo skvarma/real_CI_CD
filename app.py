41d2e599057168a50b24c244d0ad53fb1a9a31e9
skvarma <skvarma@gmail.com>
2017-06-21 12:12:05 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+"""A simple API to do almost nothing"""
+import hug
+
+
+@hug.local()
+@hug.get('/users', versions=1)
+def user(user_id):
+    return 'I do nothing useful.'
+    # return user_id
+
+if __name__ == '__main__':
+    user.interface.local()
